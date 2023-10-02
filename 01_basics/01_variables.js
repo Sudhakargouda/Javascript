d9936b508733a6c1b9216519b54390f9a4d1c580
Sudhakargouda <141037091+Sudhakargouda@users.noreply.github.com>
2023-10-02 00:10:43 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 123456
+let accountName = "ABCD"
+let accountEmail = "Abcd@google.com"
+var accountPassword = "1233455678"
+
+let accountState;
+//if we use keyword and do not assign to it then it will show undefined 
+
+/* var is not used nowdays in java beacuse due to scope means { }
+prefer not use var
+because of issue in block scope and functional scope
+*/
+
+
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountName, accountEmail, accountPassword, accountCity,accountState])
