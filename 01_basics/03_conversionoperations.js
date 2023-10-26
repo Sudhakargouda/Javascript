652a46a8143b643ef5c3107dc991b2ad9fb9d779
Sudhakargouda <141037091+Sudhakargouda@users.noreply.github.com>
2023-10-26 12:17:17 +0000

    conversions

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -14,3 +14,29 @@ let boo = ""
 let booleanBoo = Boolean(boo)
 console.log(booleanBoo)
 console.log(typeof booleanBoo);
+
+
+// ***********Operations **********
+
+//simple
+console.log(2 + 2)
+
+console.log("1" + 2)
+//In above code string number is given hence if you give first string number means the sum will not calculate
+
+console.log(1 + 2 + "3");
+// if you give at last means it will calculate 
+
+//increment and decrement
+let game = 500
+--game;
+console.log(game)
+
+
+let x = 5
+const y = x++
+console.log(x,y);
+
+let a = 10
+const b = ++a
+console.log(a, b);
