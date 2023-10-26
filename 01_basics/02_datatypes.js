3d4966c46ddd674f3d1260587b193eedb0393628
Sudhakargouda <141037091+Sudhakargouda@users.noreply.github.com>
2023-10-26 02:24:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+//data types
+/*
+number 
+String
+null //it is an empty value and standalone
+undefined
+Boolean
+symbol unique
+*/
+
+console.log(typeof null)
+console.log(typeof undefined);
+
+let name = "Sudhakar"
+let vehicleNumber = 222
+
+console.log(vehicleNumber);
