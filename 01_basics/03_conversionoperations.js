4ef841759182552e15290eefbfc644baa78ab4ac
Sudhakargouda <141037091+Sudhakargouda@users.noreply.github.com>
2023-10-26 10:54:07 +0000

    coversionof datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,16 @@
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+let playerJerseyNumber = 7
+
+let numNumber = Number(playerJerseyNumber)
+console.log(numNumber)
+console.log(typeof numNumber)
+
+
+let boo = ""
+let booleanBoo = Boolean(boo)
+console.log(booleanBoo)
+console.log(typeof booleanBoo);
