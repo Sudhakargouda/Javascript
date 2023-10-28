da62c2b5e264f6ce9c70d6fb2a08971ba248651f
Sudhakargouda <141037091+Sudhakargouda@users.noreply.github.com>
2023-10-28 10:47:26 +0000

    IIFE

---
+++ 02_basics/06_IIFE.js
@@ -0,0 +1,10 @@
+//Immediately Invoked Function Expression(IIFE)
+
+
+(function chai (){
+    console.log(`DB CONNECTED`);
+})();//semicolon is important to execute the next Function
+
+( (name) =>{
+    console.log(`DB CONNECTED ${name}`);
+})("Sudhakar")
