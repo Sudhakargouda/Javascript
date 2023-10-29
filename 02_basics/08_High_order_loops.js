7798ea813d5b765bbcb6650310085d2975359e05
Sudhakargouda <141037091+Sudhakargouda@users.noreply.github.com>
2023-10-29 16:21:32 +0000

    high

--- 02_basics/08_High_order_loops.js
+++ 02_basics/08_High_order_loops.js
@@ -19,7 +19,7 @@ map.set('In', "India")
 map.set('USA', "United state America")
 map.set('FR', "France")
 
-console.log(map);
+//console.log(map);
 
 for (const key of map) {
    // console.log(key);
@@ -58,7 +58,7 @@ for(const key in programming){
 
 //for each loop
 
-const coding = ["js", "rb", "py", "java", "cpp"]
+// const coding = ["js", "rb", "py", "java", "cpp"]
 
 //By Function
 // coding.forEach(function (val){
@@ -70,31 +70,133 @@ const coding = ["js", "rb", "py", "java", "cpp"]
 //     //console.log(item);
 // })
 
-function printMe(item){
-    console.log(item);
-}
+// function printMe(item){
+//     console.log(item);
+// }
 
-coding.forEach(printMe)
+// coding.forEach(printMe)
 
-coding.forEach((item,index,arr) =>{
-    console.log(item,index,arr);
+// coding.forEach((item,index,arr) =>{
+//     console.log(item,index,arr);
+// })
+
+// const myCoding = [
+//     {
+//         languageName: "javascript",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+//     {
+//         languageName: "Ruby",
+//         languageFileName: "Rb"
+//     }
+// ]
+
+// myCoding.forEach((item) => {
+//     console.log(item.languageName);
+// })
+
+
+const coding = ["js", "rb", "py", "java", "cpp"]
+
+
+//In forEach loop we cannot return the specfic item which we want
+const values = coding.forEach((item) => {
+    //console.log(item)
+    return item
 })
 
-const myCoding = [
-    {
-        languageName: "javascript",
-        languageFileName: "js"
-    },
-    {
-        languageName: "python",
-        languageFileName: "py"
-    },
-    {
-        languageName: "Ruby",
-        languageFileName: "Rb"
-    }
+//console.log(values);
+
+//So to do that we use filter
+
+
+//const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter((num) => {
+//      return num > 4
+// })
+
+//console.log(newNums);
+
+const books = [
+   { title : 'Book One', genre: 'Fiction', publish: 1981, edition: 2004},
+   { title : 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008},
+   { title : 'Book Three', genre: 'History', publish: 1999, edition: 2007},
+   { title : 'Book Four', genre: 'Science', publish: 1989, edition: 2009},
+   { title : 'Book Five', genre: 'Fiction', Social: 1988, edition: 2088}
 ]
 
-myCoding.forEach((item) => {
-    console.log(item.languageName);
-})
+// let mybook = books.filter((boo) => boo.genre === 'History')
+
+//  mybook = books.filter((boo) => {
+//     return boo.publish >= 1975 && boo.genre === 'Histroy'
+// })
+
+// const mybook = books.filter((boo) => {
+//     return boo.publish >= 1975 && boo.genre === 'History'
+// })
+
+// console.log(mybook);
+
+
+//map function cam also be used
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// const Nums = myNumbers.map((num) => num + 10)
+
+//console.log(Nums);
+
+
+//we can do chanining where we can more methods or Functions
+const Nums = myNumbers
+.map((num) => num + 10)
+.map((num) => num + 1)
+.filter((num) =>num >= 15)
+
+//console.log(Nums);
+
+//very important most we use this  method
+
+const myNums = [1,2,3]
+
+//const myTotal = myyNums.reduce
+
+// (function(accumalator, currentvalue) {
+//     console.log(`acc: ${accumalator} and currval: ${currentvalue}`);
+//     return accumalator + currentvalue
+// }, 0)
+
+const myTotal = myNums.reduce((acc,currval) =>acc + currval , 0)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "javascript",
+        price: 999
+    },
+    {
+        itemName: "ruby",
+        price: 1111
+    },
+    {
+        itemName: "swift",
+        price: 8967
+    },
+    {
+        itemName: "cpp",
+        price: 12988
+    },
+    {
+        itemName: "javascript",
+        price: 8775638
+    },
+]
+
+const shopping = shoppingCart.reduce((acc, item ) => acc + item.price, 0)
+
+console.log(shopping);
