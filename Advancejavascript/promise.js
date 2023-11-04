96ac4ade05207081fde95795ad02f85f531b96ed
Sudhakargouda <141037091+Sudhakargouda@users.noreply.github.com>
2023-11-04 10:15:53 +0000

    changes

--- Advancejavascript/promise.js
+++ Advancejavascript/promise.js
@@ -32,7 +32,7 @@ promiseThree.then(function (user) {
 
 const promiseFour = new Promise(function (resolve,reject) {
     setTimeout(function(){
-        let error = true
+        let error = false
         if(!error){
             resolve({username: "Sudhakar",password: "12345"});
         }else{
@@ -51,4 +51,45 @@ promiseFour.then((user) => {
 }).finally(() => console.log("the promise either resolve or rejected"))
 //In this above fourth promise we came to know that we can run multiple then and how error can occur and how to work with an error and how to pass the values and take particular objects like username
 
+
+const promiseFive = new Promise(function (resolve,reject) {
+    setTimeout(function(){
+        let error = false
+        if(!error){
+            resolve({username: "Javascript",password: "12345"});
+        }else{
+            reject('ERROR: JS went wrong')
+        }        
+    },1000)
+});
+
+//async await in first method he told it will not run because it won't shown an error by try catch block it will show an error and this method works
+async function consumepromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumepromiseFive();
+
+// async function getAllUSers(){
+//     try {
+//         const response = await fetch ('https://api.github.com/users/hiteshchoudhary')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+// getAllUSers();
+
+fetch('https://api.github.com/users/hiteshchoudhary').then((response) =>{
+    return response.json()
+}).then((data) => {
+    console.log(data);
+}).catch((error) => console.log(error))
 
